de11789dfe377bd48515d993e2b10bc20beddbee
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-06-14 18:00:06 +0300

    Files updated

--- settings.py
+++ settings.py
@@ -7,6 +7,9 @@ class Settings:
 
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (4, 2, 40)
+        self.bg_color = (4, 2, 80)
         self.ship_speed = 1.5
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 156
 
