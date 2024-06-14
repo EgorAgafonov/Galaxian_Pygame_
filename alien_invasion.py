3586129979657edac1cc84999ae6c35832102dfd
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-06-14 16:58:13 +0300

    New files added

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,7 +29,6 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
-
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
@@ -40,6 +39,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
 
     def _check_keyup_events(self, event):
         """Реагирует на событие возвращения клавиши вверх (KEYUP)"""
@@ -48,7 +51,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
--- ship.py
+++ ship.py
@@ -14,8 +14,11 @@ class Ship:
         self.screen_rect = self.screen.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Refreshes user's ship position according the flag"""
@@ -23,8 +26,13 @@ class Ship:
             self.x += self.ai_settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.ai_settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.ai_settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.ai_settings.ship_speed
 
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Displays the ship at its current position."""
