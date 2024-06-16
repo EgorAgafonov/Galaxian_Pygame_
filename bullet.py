dbf2ea3fa942f66ad9e372bedf610a4948fcc88f
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-06-16 16:00:27 +0300

    Files updated

--- bullet.py
+++ bullet.py
@@ -5,12 +5,14 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """Создает объект снаряда в текущей позиции корабля"""
 
-    def __init__(self, ai_settings, screen):
+    def __init__(self, ai_game):
         super().__init__()
 
-        self.screen = screen
-        self.settings = ai_settings
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.color = self.settings.bullet_color
     # создание снаряда в позиции (0, 0) и назначение правильной позиции
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
-        self.rect.midtop =
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        self.y = float(self.rect.y)
