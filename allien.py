b0fe93798763f10ab280d24555ffe668c9b7cfc6
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-06-17 22:09:47 +0300

    Files updated

---
+++ allien.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Alien's main battle-ship model"""
+
+    def __init__(self, ai_game):
+        """Initialize user's main battle-ship and sets starting position."""
+
+        self.screen = ai_game.screen
+        self.image = pygame.image.load('images/alien_ship.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
